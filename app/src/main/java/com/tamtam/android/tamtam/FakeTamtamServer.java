1fbf53b0c4701b3a0a4d32f92c29900cb6159d05
antoninpa <antonin.perrotaudet@orange.com>
2017-01-13 18:20:33 +0100

    initial work on model objects repositories to mock network interactions

--- app/src/main/java/com/tamtam/android/tamtam/FakeTamtamServer.java
+++ app/src/main/java/com/tamtam/android/tamtam/FakeTamtamServer.java
@@ -9,8 +9,25 @@ import static android.view.View.X;
 
 public class FakeTamtamServer {
 
-    public String thingString1 = "{\"thingId\":\"Thing1\",\"pict\":\"AAaaaIaAMaBASEa64aENCODEDaaaag==\",\"description\":\"cest un premier truc\",\"price\":{\"currency\":489,\"price\":60},\"position\":{\"lon\":7.05289,\"lat\":43.6166},\"stuck\":false}";
-    public String thingString2 = "{\"thingId\":\"Thing2\",\"pict\":\"AAaaaIaAMaBASEa64aENCODEDaaaag==\",\"description\":\"cest un troisieme truc\",\"price\":{\"currency\":489,\"price\":60},\"position\":{\"lon\":7.05334,\"lat\":43.61664},\"stuck\":false}";
-    public String thingString3 = "{\"thingId\":\"Thing3\",\"pict\":\"AAaaaIaAMaBASEa64aENCODEDaaaag==\",\"description\":\"cest un troisieme truc\",\"price\":{\"currency\":489,\"price\":60},\"position\":{\"lon\":7.12153,\"lat\":43.65839},\"stuck\":false}";
+    public final String thingString1 = "{\"thingId\":\"thing1\",\"pict\":\"AAaaaIaAMaBASEa64aENCODEDaaaag==\",\"description\":\"cest un premier truc\",\"price\":{\"currency\":489,\"price\":10.10},\"position\":{\"lon\":7.05289,\"lat\":43.6166},\"stuck\":false}";
+    public final String thingString2 = "{\"thingId\":\"thing2\",\"pict\":\"AAaaaIaAMaBASEa64aENCODEDaaaag==\",\"description\":\"cest un deuxieme truc\",\"price\":{\"currency\":489,\"price\":20.20},\"position\":{\"lon\":7.05334,\"lat\":43.61664},\"stuck\":false}";
+    public final String thingString3 = "{\"thingId\":\"thing3\",\"pict\":\"AAaaaIaAMaBASEa64aENCODEDaaaag==\",\"description\":\"cest un troisieme truc\",\"price\":{\"currency\":489,\"price\":30.30},\"position\":{\"lon\":7.12153,\"lat\":43.65839},\"stuck\":false}";
 
+    public String getThing(String thingId) {
+        if (thingId.equals("thing1")){
+            return thingString1;
+        } else if (thingId.equals("thing2")){
+            return thingString2;
+        } else if (thingId.equals("thing3")){
+            return thingString3;
+        } else {
+            return null;
+        }
+    }
+
+    public String getThingJsonList() {
+        return "["+thingString1 + "," +
+                thingString2 + "," +
+                thingString3 + "," + "]";
+    }
 }
---
+++ app/src/main/java/com/tamtam/android/tamtam/services/Specification.java
@@ -0,0 +1,8 @@
+package com.tamtam.android.tamtam.services;
+
+/**
+ * Created by fcng1847 on 13/01/17.
+ */
+
+public interface Specification {
+}
